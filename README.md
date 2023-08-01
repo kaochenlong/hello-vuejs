0df842cc1b3cf73a0b2f133a42bb78bea49ff2f1
Eddie Kao <eddie@5xcampus.com>
2023-08-02 04:52:18 +0800

    install taliwind with daisy ui

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 練手用專案
 
+- Vue 3.3.4
+- Tailwind CSS 3.3.3
+
 by eddie@5xcampus.com
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script></script>
+
+<template>
+  <div class="navbar bg-base-100 border-b-2">
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl">Hello Vue.js</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>連結 1</a></li>
+        <li><a>連結 2</a></li>
+        <li><a>連結 3</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
