3c30eb0b447d8df802be54198614ee2b4eb9a147
Eddie Kao <eddie@5xcampus.com>
2023-08-02 05:24:34 +0800

    add Home view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script>
 import Navbar from "./components/Navbar.vue"
+import Home from "./components/Home.vue"
 
 export default {
-  components: { Navbar },
+  components: { Navbar, Home },
 }
 </script>
 
 <template>
-  <navbar></navbar>
-  <main class="container mx-auto mt-12">
-    <h1 class="font-thin text-6xl">Hello World，哈囉世界！</h1>
-  </main>
+  <navbar />
+  <home />
 </template>
 
 <style scoped></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <main class="container mx-auto mt-12">
+    <h1 class="font-thin text-6xl">Hello World，哈囉世界！</h1>
+  </main>
+</template>
+
+<style scoped></style>
